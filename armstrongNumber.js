84296e1944a33a62e275eb45900ee6ed714050f0
Tim Ludwiczak <tim.ludwiczak@gmail.com>
2024-06-10 07:55:12 -0500

    armstrong update

--- armstrongNumber.js
+++ armstrongNumber.js
@@ -1,9 +1,11 @@
 // How can you make this more scalable and reusable later?
 
 function armstrongNumber(num){
-    let result = 0
-    for (let i = 1; i == num, i++) {
-        
+    const numStr = num.toString();
+    const numDigits = numStr.length;
+    let sum =0;
+    for (let i = 1; i == num; i++) {
+
     }
 
 }
