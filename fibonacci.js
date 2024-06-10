eb9da88f931bc0912563d219f99defc26af23b83
Tim Ludwiczak <tim.ludwiczak@gmail.com>
2024-06-10 07:17:53 -0500

    fibonacci complete

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,16 @@
 function fibonacci(num) {
-  let result = new Array(n + 1);
-  for( let i = 2; i <= num, i++;) {
+  
+  if (num === 0) return 0;
+  if (num === 1) return 1;
+
+  let result= new Array(num + 1);
+  result[0] = 0;
+  result[1] = 1;
+
+  for( let i = 2; i <= num; i++) {
     result[i] = result[i-1] + result[i-2];
   }
-  return 0;
+  return result[num];
 }
 
 console.log(fibonacci(7));
