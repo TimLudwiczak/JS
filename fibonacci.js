d5b2288d9a89cc97db5092899cc0b1f11176c0f8
Tim Ludwiczak <tim.ludwiczak@gmail.com>
2024-06-09 19:39:48 -0500

    Adding Fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,6 @@
+function fibonacci(num) {
+  let result = 1
+  return 0;
+}
+
+module.exports = fibonacci;
---
+++ toRomanLazy.js
@@ -0,0 +1,6 @@
+function toRomanLazy(num) {
+    let output = "";
+    // romanNumeralToArabic
+    let romanNumeralPriorityOrder = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
+    
+}
